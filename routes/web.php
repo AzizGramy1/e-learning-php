0a39b38ed615f3d40adcef40597e14d90d75a057
AzizGramy1 <bay074682@gmail.com>
2025-03-27 23:22:13 +0100

    installation de laravel/ui package et rectifications des routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,31 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CertificatController;
+use App\Http\Controllers\CoursController;
+use App\Http\Controllers\UtilisateurController;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
+
+// Route d'accueil
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*
+|--------------------------------------------------------------------------
+| Routes CRUD
+|--------------------------------------------------------------------------
+*/
+
+// Routes pour les certificats
+Route::resource('certificats', CertificatController::class)
+    ->except(['show']) // Exclure la route show si non utilisée
+    ->middleware('auth'); // Protection par authentification
+
+
+
